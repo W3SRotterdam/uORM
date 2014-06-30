84b2e745b94056389736d55ea1b1ce28c395fa98
W3S-uORM <ed@w3s.nl>
2014-06-30 13:03:28 +0200

    UmbracoApplications veranderd

---
+++ uORM/Models/W3S/uORM/uApplication.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Umbraco.Core;
+using Umbraco.Core.Services;
+
+namespace W3S.uORM {
+    public class uApplication : ApplicationEventHandler {
+        public uApplication() { }
+        protected override void ApplicationStarted(UmbracoApplicationBase httpApplication, ApplicationContext applicationContext) {
+            base.ApplicationStarting(httpApplication, applicationContext);
+
+            ModelBinders.Binders.DefaultBinder = new W3S.uORM.RenderModelBinder();
+        }
+    }
+}
